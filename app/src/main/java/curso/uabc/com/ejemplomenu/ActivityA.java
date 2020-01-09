a91bcadc1cced4d7f1619e98bc2a4c7b7fa63910
xhendor <rosendorsc@gmail.com>
2020-01-09 15:28:43 -0800

    Actualización AndroidX

--- app/src/main/java/curso/uabc/com/ejemplomenu/ActivityA.java
+++ app/src/main/java/curso/uabc/com/ejemplomenu/ActivityA.java
@@ -1,7 +1,7 @@
 package curso.uabc.com.ejemplomenu;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import androidx.appcompat.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
--- app/src/main/java/curso/uabc/com/ejemplomenu/MainActivity.java
+++ app/src/main/java/curso/uabc/com/ejemplomenu/MainActivity.java
@@ -1,7 +1,7 @@
 package curso.uabc.com.ejemplomenu;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import androidx.appcompat.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,8 +13,6 @@ import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-
-import java.util.HashMap;
 
 
 public class MainActivity extends ActionBarActivity implements
